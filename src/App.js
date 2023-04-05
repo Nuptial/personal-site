2e29015c5df1d0d73dbcd944d62e4a00433de828
Nuptial <alican.camlibel91@gmail.com>
2023-04-05 16:14:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import Typed from "typed.js";
+import { useEffect, useRef, useState } from "react";
+import { SocialIcon } from "react-social-icons";
+
+import "./App.css";
 
 function App() {
+  const [visibleSocialIcons, setVisibleSocialIcons] = useState(false);
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Welcome to my personal website.",
+        "My name is Alican Çamlıbel.",
+        "I have started the software development adventure since 2017.",
+        "I'm working as Frontend Developer since 2017 I try to follow up-to-date technologies related to software, mostly frontend.",
+        " You can follow the links below to get to know me better and follow my accounts.",
+      ],
+      typeSpeed: 30,
+      backSpeed: 30,
+      backDelay: 500,
+      startDelay: 1000,
+      showCursor: false,
+      onComplete: () => {
+        const animationText =
+          document.getElementsByClassName("animation-text")[0];
+
+        animationText.classList.add("moveTop");
+        setVisibleSocialIcons(true);
+      },
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span ref={el} className="animation-text"></span>
+      {visibleSocialIcons && (
+        <>
+          <SocialIcon
+            url="https://github.com/Nuptial"
+            target="_blank"
+            bgColor="white"
+            id="github-icon"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/alican-%C3%A7aml%C4%B1bel-7a160290/"
+            target="_blank"
+            bgColor="white"
+            id="linkedin-icon"
+          />
+          <SocialIcon
+            url="https://medium.com/@alican.camlibel91"
+            target="_blank"
+            bgColor="white"
+            id="medium-icon"
+          />
+        </>
+      )}
     </div>
   );
 }
